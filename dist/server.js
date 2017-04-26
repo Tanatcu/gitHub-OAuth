eb9b1613e5cfe0cbc7461b94e01579e4d5afcdc7
Ruslan Loboda <tanatcu.iuko@gmail.com>
2017-04-26 23:41:56 +0300

    added headers CORS

--- dist/server.js
+++ dist/server.js
@@ -7,6 +7,12 @@ app.use(history({}));
 
 app.use(serveStatic(__dirname));
 
+app.all('/', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
+
 var port = process.env.PORT || 5000;
 
 app.listen(port);
