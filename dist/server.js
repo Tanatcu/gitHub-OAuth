01491486f6d9e5a590a09be8bed44a19b1bbc018
Ruslan Loboda <tanatcu.iuko@gmail.com>
2017-04-28 02:45:04 +0300

    test cors

--- dist/server.js
+++ dist/server.js
@@ -9,8 +9,13 @@ app.use(serveStatic(__dirname));
 
 app.all('/', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	next();
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+	if(req.method === "OPTIONS")
+		res.send(200)
+	else
+		next();
 });
 
 var port = process.env.PORT || 5000;
